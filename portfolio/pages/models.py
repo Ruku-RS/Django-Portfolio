28064ad1f88eda54c8794e62b100637292ee0ee3
Rukman <rukmansubedi@gmail.com>
2026-08-05 10:03:04 +0545

    installed Pillow, and also tested database in shell

--- portfolio/pages/models.py
+++ portfolio/pages/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    github_link = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to="projects/",blank=True, null=True)
+    is_featured = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
