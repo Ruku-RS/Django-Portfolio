ac25e7525963116c30be63fbee9c9aba694049f6
Rukman <rukmansubedi@gmail.com>
2026-08-05 10:24:31 +0545

    Created a superuser and managed Project file

--- portfolio/pages/admin.py
+++ portfolio/pages/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Project
 
 # Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display=(
+        "title",
+        "github_link",
+        "created_at",
+    )
+
+    search_fields=(
+        "title",
+        "description",
+    )
+
+    list_filter=(
+        "created_at",
+    )
