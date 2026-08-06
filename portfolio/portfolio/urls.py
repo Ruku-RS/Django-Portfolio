2b0315fccb5de10b12859bf728df02c6849eac38
Rukman <rukmansubedi@gmail.com>
2026-08-06 21:02:32 +0545

    Use of Media files to dynamic upload of images in the projects page

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -16,9 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from pages import views
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path("", include("pages.urls")),
     path("admin/", admin.site.urls),
+    path("", include("pages.urls")),
+    
 ]
+
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root= settings.MEDIA_ROOT
+)
