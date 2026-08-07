d686b0754d6f9f8aadb29222e2f5b5e181c58665
Rukman <rukmansubedi@gmail.com>
2026-08-07 16:44:15 +0545

    Login and register page setup for user

--- portfolio/accounts/views.py
+++ portfolio/accounts/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import (UserCreationForm, AuthenticationForm)
+from django.contrib.auth import login
 from django.contrib import messages
 
 # Create your views here.
+
+# Register View
 def register(request):
     if request.method == 'POST':
         form =UserCreationForm(request.POST)
@@ -15,3 +18,17 @@ def register(request):
     else:
         form= UserCreationForm()
     return render(request, "accounts/register.html", {"form": form})
+
+# Login View
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user= form.get_user()
+            
+            login(request, user,)
+            messages.success(request, f"Welcome back, {user.username}!",)
+            return redirect('home')
+    else:
+        form =AuthenticationForm()
+    return render(request, "accounts/login.html",{"form":form})        
