9626e6a73fd59363d9d1ece0c0bd26e35bb9c19d
Rukman <rukmansubedi@gmail.com>
2026-08-07 20:57:05 +0545

    Profile creation of user setup

--- portfolio/accounts/models.py
+++ portfolio/accounts/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User, 
+        on_delete=models.CASCADE,
+    )
+    bio=models.TextField(
+        blank=True,
+    )
+    location = models.CharField(
+        max_length=100,
+        blank=True,
+    )
+    website =models.URLField(
+        blank=True,
+    )
+    image= models.ImageField(
+        upload_to="profiles/",
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.user.username
--- portfolio/accounts/urls.py
+++ portfolio/accounts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("register/", views.register, name='register'),
@@ -7,3 +9,5 @@ urlpatterns = [
     path("logout/",views.user_logout, name="logout"),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
