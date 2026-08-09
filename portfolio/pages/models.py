0e951d2c5c9f508c6180333b1e0f964f5f7f0e22
Rukman <rukmansubedi@gmail.com>
2026-08-09 16:48:58 +0545

    added owner table to the db

--- portfolio/pages/models.py
+++ portfolio/pages/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 #Project page
 class Project(models.Model):
-    title = models.CharField(max_length=100)
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='projects',
+        
+    )
+
+    title = models.CharField(max_length=200)
 
     description = models.TextField()
 
